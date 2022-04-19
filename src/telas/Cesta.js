acadd5e1451b40f1d5a67ed8faff373879d81639
Fernando de Lima <brmou0146@ambev.com.br>
2022-04-18 21:50:05 -0300

    Primeira tela com imagem e titulo por cima

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { StyleSheet, Image, Dimensions, Text } from "react-native";
 
-export default function Cesta(){
-    return <Text>Cesta</Text>
+import topo from "../../assets/topo.png";
+
+const width = Dimensions.get("screen").width;
+
+export default function Cesta() {
+  return (
+    <>
+      <Image source={topo} style={estilos.topo} />
+      <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+    </>
+  );
 }
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    //Altura da Imagem / Largura da imagem * Largura da tela
+    height: (578 / 768) * width,
+  },
+  titulo: {
+    //define um componente sobre o outro
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 18,
+    lineHeight: 30,
+    color: "white",
+    fontWeight: "bold",
+    padding: 18
+  },
+});
